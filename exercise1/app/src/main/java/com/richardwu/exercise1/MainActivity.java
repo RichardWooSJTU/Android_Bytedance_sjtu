80d58adb500a8aa8f4996d857e00a6bceb1b3f75
RichardWooSJTU <37864677+RichardWooSJTU@users.noreply.github.com>
2020-11-15 21:05:09 +0800

    design search layout

--- exercise1/app/src/main/java/com/richardwu/exercise1/MainActivity.java
+++ exercise1/app/src/main/java/com/richardwu/exercise1/MainActivity.java
@@ -3,6 +3,10 @@ package com.richardwu.exercise1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SearchLayout searchLayout = findViewById(R.id.search_layout);
+        EditText searchValue = findViewById(R.id.search_value);
+        Button cancelButton = findViewById(R.id.cancel_button);
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                searchValue.setText("");
+            }
+        });
     }
 }
--- exercise1/app/src/main/java/com/richardwu/exercise1/SearchLayout.java
+++ exercise1/app/src/main/java/com/richardwu/exercise1/SearchLayout.java
@@ -1,4 +1,34 @@
 package com.richardwu.exercise1;
 
-public class SearchLayout {
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class SearchLayout extends FrameLayout {
+    public SearchLayout(@NonNull Context context) {
+        super(context);
+        init();
+    }
+
+    public SearchLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public SearchLayout(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    public SearchLayout(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init();
+    }
+
+    private void init() {
+        inflate(getContext(),R.layout.layout_search,this);
+    }
 }
